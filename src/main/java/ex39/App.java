1ddf51f84d264077fa45c85fb39b0283f1dd0f9b
Christian Williams <christian.b.williams@gmail.com>
2021-09-27 23:14:00 -0400

    Final Push

--- src/main/java/ex39/App.java
+++ src/main/java/ex39/App.java
@@ -5,15 +5,47 @@
 
 package ex39;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
-public class App {
+public class App { // im a bit confused on this map stuff and im out of time to finish the last 2 exercises
     public static void main( String[] args ){
-        Scanner input = new Scanner(System.in);
+        List<Employees> employee = new ArrayList<Employees>();
 
-        System.out.print("Enter two strings and I'll tell you if they are anagrams:\nEnter the first string: ");
-        String str1 = input.nextLine();
-        System.out.print("Enter the second string: ");
-        String str2 = input.nextLine();
+        employee.add(new Employees("John", "Johnson", "Manager", "2016-12-31"));
+        employee.add(new Employees("Tou", "Xiong", "Software Engineer", "2016-10-05"));
+        employee.add(new Employees("Michaela", "Michaelson", "District Manager", "2015-12-19"));
+        employee.add(new Employees("Jake", "Jacobson", "Programmer", ""));
+        employee.add(new Employees("Jacquelyn", "Jackson", "DBA", ""));
+        employee.add(new Employees("Sally", "Webber", "Web Developer", "2015-12-18"));
+    }
+}
+
+class Employees {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String date;
+
+    public Employees(String firstName, String lastName, String position, String date) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.date = date;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public String getDate() {
+        return date;
     }
 }
